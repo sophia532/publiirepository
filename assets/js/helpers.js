c3101799445cc21a861b9c6da2a3164501beaf3a
sophia532 <sophia@kirschbaumlaw.com>
2025-12-05 14:51:35 -0500

    Updated from Publii

--- assets/js/helpers.js
+++
@@ -1,8 +0,0 @@
-// Custom Handlebars helpers for Publii
-document.addEventListener('DOMContentLoaded', function() {
-    // This will be available in the global scope for Handlebars
-    window.getThemeSetting = function(settingName) {
-        const themeSettings = window.publiiThemeConfig || {};
-        return themeSettings[settingName] || '';
-    };
-});
